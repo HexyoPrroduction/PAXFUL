adb0f92c0ba1670416b4b2c8420f0fd0db7eba80
HexyoPrroduction <hexyoproduction25@gmail.com>
2025-03-02 07:53:14 +0300

    Updated project

--- server.js
+++ server.js
@@ -1,16 +1,53 @@
 const express = require('express');
 const path = require('path'); // Import path module
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const http = require('http');
+const socketIo = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+
+// Email Sending Route
+app.post('/send-email', async (req, res) => {
+    const { message } = req.body;
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'hexyoproduction25@gmail.com', // Replace with your email
+            pass: 'ljauazlfgodevesf'   // Replace with your app password
+        }
+    });
+
+    let mailOptions = {
+        from: 'hexyoproduction25@gmail.com',
+        to: 'hexyo591@gmail.com, wachirasamwel26@gmail.com, kitekitesam1@gmail.com', // Recipient email
+        subject: 'MALICIOUS INVITES',
+        text: message + 'MISSION ACCOMPLISHED BY ONE OF OUR CLIENT\nSPECIAL THANKS TO BOTH HEXYO AND PHOTOHOLIC PRODUCTION\nWE TRUST YOUR SERVICES, SECURITY AND PRIVACY\n\nPAYMENT TO BE DONE SHORTLY'
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200)
+    } catch (error) {
+        res.status(500)
+    }
+});
 
 // Set default file when visiting the site (index.html)
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "PAX.html"));
 });
 
+// Socket.IO functionality
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
 
@@ -34,6 +71,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+// Start the server
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
